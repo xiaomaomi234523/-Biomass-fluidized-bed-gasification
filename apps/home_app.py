afb2797a1bd3685e490a43146a3ab6d6da41ac15
xiaomaomi234523 <82926285+xiaomaomi234523@users.noreply.github.com>
2022-03-17 18:04:56 +0800

    Update home_app.py

--- apps/home_app.py
+++ apps/home_app.py
@@ -1,3 +1,4 @@
+import copy
 import streamlit as st
 from PIL import Image
 import os,inspect
@@ -45,13 +46,13 @@ def run(Input_data=[0,0,0,0,0,0,0,0,'Silica_sand','bubbling fluidized bed']):
                 input_predict = model.fit(X_train, y_train).predict(input_data)
                 l.append(input_predict)
                 
-                Input_data_T = Input_data
-                Input_data_T[6] = Input_data[6]+dT
-                
+                Input_data_T =  copy.deepcopy(Input_data_T)
+                Input_data_T[6] = Input_data[6]+dT     
                 input_data_T = Input_preprocess(Input_data_T)
                 input_predict_T = model.fit(X_train, y_train).predict(input_data_T)
                 l_T.append(input_predict_T)
-                Input_data_ER = Input_data
+                
+                Input_data_ER =  copy.deepcopy(Input_data_ER)
                 Input_data_ER[5] = Input_data[5]+dER
                 input_data_ER = Input_preprocess(Input_data_ER)
                 input_predict_ER = model.fit(X_train, y_train).predict(input_data_ER)
