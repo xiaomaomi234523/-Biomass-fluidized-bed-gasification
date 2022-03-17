6ff14908408d001e3edd84674482821f982ece49
xiaomaomi234523 <82926285+xiaomaomi234523@users.noreply.github.com>
2022-03-17 13:38:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ elif input_mode == "单组数据":
 
 # Once we have the dependencies, add a selector for the app mode on the sidebar.
 st.sidebar.title("选择页面")
-app_mode = st.sidebar.selectbox("",("home","Lasso","SVR","ANN","XGB","Ridge", "ElasticNet","GBDT","RandomForest","Adaboost"))
+app_mode = st.sidebar.selectbox("",("home","Lasso","SVR","ANN","Ridge", "ElasticNet","GBDT","RandomForest","Adaboost"))
 if app_mode == "home":
     home_app.run(Input_data=Input_data)
     st.sidebar.success('请选择模型并试着调参吧!".')
