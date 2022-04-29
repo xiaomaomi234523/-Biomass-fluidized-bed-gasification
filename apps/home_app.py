3ed5a43c9306ae07ef3d9d6dbe14d3508998b547
xiaomaomi234523 <82926285+xiaomaomi234523@users.noreply.github.com>
2022-04-29 17:21:01 +0800

    Update home_app.py

--- apps/home_app.py
+++ apps/home_app.py
@@ -96,6 +96,7 @@ def run(Input_data=[0,0,0,0,0,0,0,0,'Silica_sand','bubbling fluidized bed'],Modl
             input_data = Input_preprocess(Input_data)
             input_predict = model.fit(X_train, y_train).predict(input_data)
             l.append(input_predict)
+        st.write(l)
         i = 0
         for target in ["CO", "H2", "CH4", "CO2"]:
             st.write(target, '[%vol_N2_free]=', l[i][0] / sum(l)[0] * 100, '%')
