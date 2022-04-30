f406ce7b55b36f04ff19326c9ff7b68d02502d2f
xiaomaomi234523 <82926285+xiaomaomi234523@users.noreply.github.com>
2022-04-30 16:57:06 +0800

    Update SVR_app.py

--- apps/SVR_app.py
+++ apps/SVR_app.py
@@ -68,7 +68,7 @@ def run(Input_data=[0,0,0,0,0,0,0,0,'Silica_sand','bubbling fluidized bed'],Modl
         input_predict = model.fit(X_train, y_train).predict(input_data)
         l.append(input_predict)
         l = pd.DataFrame(l).T
-        dic = {target:l[0]}
+        dic = {target+[%vol_N2_free]:l[0]}
         st.write(dic)
         df = pd.DataFrame(dic)
         st.write(df)
