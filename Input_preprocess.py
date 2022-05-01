65784f61e0600340f6decf0f8909b9b008345cdf
xiaomaomi234523 <82926285+xiaomaomi234523@users.noreply.github.com>
2022-05-01 21:50:39 +0800

    Update Input_preprocess.py

--- Input_preprocess.py
+++ Input_preprocess.py
@@ -31,15 +31,11 @@ def Input_preprocess(Input_data):
 
     #  跳过去除异常值，开始下面的步骤
     data = load_csv("x_data_no_outlier.csv")
-    st.write(data)
-    st.write(data.shape[0])
     l1 = int(0.8 * data.shape[0])
     l2 = data.shape[0] - l1
     train_data = data.head(l1)
     test_data = data.tail(l2)
     test_data = pd.concat([test_data,input_data])
-    st.write(train_data)
-    st.write(test_data)
     #  归一化
     from sklearn import preprocessing
 
